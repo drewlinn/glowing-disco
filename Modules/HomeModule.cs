12e9a16ef7c2b38930bb169f0ebbf7ad2884ebcb
Andrew <expandrew@gmail.com>
2017-05-26 08:41:05 -0700

    incorporate all necessary dependencies in project.json and Startup.cs, perform a dnu restore and dnu build, and begin writing the program in HomeModule.cs

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+class Contact
+{
+  private string _name;
+  private string _phone;
+  private string _address;
+  private string _email;
+}
+
+class AddressBook
+{
+  public static void Main()
+  {
+    
+  }
+}
