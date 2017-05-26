1be573bfabbe6ebb7a50feb2065f94e4a3d59e2d
Andrew <expandrew@gmail.com>
2017-05-26 09:44:50 -0700

    write most of our code in contacts.cs, index.cshtml and added_contact.cshtml, and also add clear_contacts.cshtml file.

--- Objects/contact.cs
+++ Objects/contact.cs
@@ -2,11 +2,58 @@ using System;
 using System.Collections.Generic;
 
 namespace AddressBook.Objects
-
-public class Contact
 {
-  private string _name;
-  private string _phone;
-  private string _address;
-  private string _email;
+
+  public class Contact
+  {
+    private string _name;
+    private string _phone;
+    private string _address;
+    private string _email;
+  
+    public Contact(string name, string phone, string address, string email)
+    {
+      _name = name;
+      _phone = phone;
+      _address = address;
+      _email = email;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+    public string GetEmail()
+    {
+      return _email;
+    }
+    public void SetEmail(string newEmail)
+    {
+      _email = newEmail;
+    }
+    public void Save()
+    {
+      _addressBook.Add(this);
+    }
+  }
 }
