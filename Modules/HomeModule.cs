71f018fb6ba5b82ff00043749897bd3730862cc0
Andrew <expandrew@gmail.com>
2017-05-28 17:32:31 -0700

    fix bug where contacts were not successfully submitting and start working on clearing contacts.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,22 +9,22 @@ namespace AddressBook
     public HomeModule()
     {
       Get["/"] = _ => {
-        return View["index.cshtml"];
-      };
-      Get["/contacts"] = _ => {
         List<Contact> allContacts = Contact.GetAll();
         return View["contacts.cshtml", allContacts];
       };
       Get["/contact/new"] = _ => {
         return View["contact_form.cshtml"];
       };
+      Get["/contacts/empty"] = _ => {
+        return View["/contacts_cleared.cshtml"]
+      }
       Post["/contacts"] = _ => {
         Contact newContact = new Contact(Request.Form["new-name"], Request.Form["new-phone"], Request.Form["new-address"], Request.Form["new-email"]);
         return View["contact_added.cshtml", newContact];
       };
       Post["/contacts/clear"] = _ => {
-        // List<Contact> allContacts = Contact.ClearAll();
-        return View["/contacts_cleared"];
+        List<Contact> allContacts = Contact.ClearAll();
+        return View["/clear_contacts.cshtml"];
       };
     }
   }
